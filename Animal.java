995023c0c8690aa6514aa3a817cc6aef7ebc4609
ThaiBao0399 <thaibaoo100@gmail.com>
2025-02-17 00:17:01 +0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,26 @@
+package Ecercise2;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Animal {
+
+    String id;
+    String source;
+    Date dateOfBirth;
+    String color;
+
+    public void makeSound() {
+    }
+
+    public void displayInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Source: " + source);
+        System.out.println("Date of Birth: " + dateOfBirth);
+        System.out.println("Color: " + color);
+
+    }
+    
+}
--- Processor.java
+++ Processor.java
@@ -1,28 +1,33 @@
+package Ecercise2;
 
-package OOP;
-
-import java.util.ArrayList;
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Scanner;
 
 public class Processor {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Car> cars = new ArrayList<>();
 
-        for (int i = 1; i <= 2; i++) {
-            System.out.println("Enter details for myCar" + i);
-            System.out.print("Model: ");
-            String model = scanner.nextLine();
-            System.out.print("Year: ");
-            int year = scanner.nextInt();
-            scanner.nextLine();
-            cars.add(new Car(model, year));
-        }
+        Dog dog = new Dog();
+        dog.id = "D001";
+        dog.source = "Breeder";
+        dog.dateOfBirth = Date.from(LocalDate.of(2022, 5, 20).atStartOfDay(ZoneId.systemDefault()).toInstant());
 
-        System.out.println("All Cars:");
-        for (Car car : cars) {
-            car.displayDetails();
-        }    
+        dog.color = "Brown";
+        dog.breed = "Golden Retriever";
+
+        Cat cat = new Cat();
+        cat.id = "C001";
+        cat.source = "Adoption";
+        cat.dateOfBirth = Date.from(LocalDate.of(2022, 5, 20).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        cat.color = "Black";
+
+        AnimalList animalList = new AnimalList();
+        animalList.addAnimal(dog);
+        animalList.addAnimal(cat);
+
+        animalList.displayAll();
     }
 }
